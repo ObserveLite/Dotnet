4746d5de7b4f3fb967dfe1240643e1dccd77594f
ObserveLite <149372161+ObserveLite@users.noreply.github.com>
2024-05-30 21:00:06 +0530

    Add files via upload

---
+++ is1.ps1
@@ -0,0 +1,16 @@
+# Download the module
+$module_url = "https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/releases/latest/download/OpenTelemetry.DotNet.Auto.psm1"
+$download_path = Join-Path $env:temp "OpenTelemetry.DotNet.Auto.psm1"
+Invoke-WebRequest -Uri $module_url -OutFile $download_path -UseBasicParsing
+
+# Import the module to use its functions
+Import-Module $download_path
+
+# Install core files
+Install-OpenTelemetryCore
+
+# Setup IIS instrumentation
+Register-OpenTelemetryForIIS
+
+
+
